1b633131f843b5ed433af78a04079bbb78c8cefd
n-atsushi <atsushi.nagami@experiencesapporo.com>
2024-05-06 15:38:38 +0900

    FIX client code.

--- client/models/client_model.py
+++ client/models/client_model.py
@@ -3,7 +3,6 @@ from client.models.client import Client
 
 class ClientModel:
     def __init__(self, user_name, token):
-        print('bbbbbb')
         self.client = Client(user_name, token)
 
     def get_token(self):
