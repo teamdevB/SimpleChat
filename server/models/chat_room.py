7f40dfe3c930712efad660a921ea1b591d6c5df9
LuxGram <104208592+LuxGram@users.noreply.github.com>
2024-05-24 23:01:49 +0900

    Refactor ChatRoom class to remove unused methods and update UDP address handling

--- server/models/chat_room.py
+++ server/models/chat_room.py
@@ -16,21 +16,9 @@ class ChatRoom:
     def is_password_checked(self, password):
         return self.room_password == password
 
-    def add_client_token(self, client_token):
-        if client_token not in self.token_list:
-            self.token_list.append(client_token)
-
-    def remove_client_info(self, client_info):
-        self.client_infos.remove(client_info)
-
     def add_udp(self,  udp_addr):
         if udp_addr not in self.address:
             self.address.append(udp_addr)
+            
     def get_udp_list(self):
         return self.address
-    
-    def get_token_list(self):
-        return self.token_list
-    
-    def get_hash_token_udp(self):
-        return self.hash_map_token_udp
