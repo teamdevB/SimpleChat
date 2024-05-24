cf05612cccf43a4b4c32839983a89b49987faf2c
LuxGram <104208592+LuxGram@users.noreply.github.com>
2024-05-24 15:40:04 +0900

    chore: Refactor server_model.py for UDP server implementation

--- server/models/server_model.py
+++ server/models/server_model.py
@@ -7,7 +7,7 @@ import configparser
 from server.views.server_view import ServerView
 import json
 import logging
-
+import uuid
 # 設定ファイル読み込み
 config = configparser.ConfigParser()
 config.read('./settings/config.ini', encoding='utf-8')
@@ -46,66 +46,41 @@ class ServerModel:
         return connection, address
 
     def create_or_join_server_room_prompt(self, client_connection):
-        ## 受信
-        client_request = self.tcp.receive_message(client_connection)
-        #self.tcp.send_request(client_connection, client_request)
-        
-        if client_request['operation'] == 1:
-            #create room
-            if not self.chat_room_list.check_room_name(client_request['room_name']):
-                self.chat_room_list.add_room(client_request['room_name'])
+        try:
+            client_request = self.tcp.receive_message(client_connection)
+            if client_request['operation'] == 1:
+                self.create_room(client_connection, client_request)
+            elif client_request['operation'] == 2:
+                self.join_room(client_connection, client_request)
+            else:
+                print("Invalid operation request.")
+        except Exception as e:
+            print(f"Error handling client request: {e}")
 
-                self.chat_room_list.set_room_password(
-                    client_request["room_name"]
-                    ,client_request["password"])
+    def create_room(self, client_connection, client_request):
+        if not self.chat_room_list.check_room_name(client_request['room_name']):
+            self.chat_room_list.add_room(client_request['room_name'])
+            self.chat_room_list.set_room_password(client_request["room_name"], client_request["password"])
+            chat_room = self.chat_room_list.get_room(client_request['room_name'])
+            chat_room.user_list.add_user(client_request['user_name'])
+            client_request['state'] = 1
+            client_request["roomPassword"] = ""
+            client_request['token'] = client_request['user_name'] + self.generate_token()
+            self.tcp.send_request(client_connection, client_request)
+            client_request = self.tcp.receive_message(client_connection)
 
-                chat_room = self.chat_room_list.get_room(client_request['room_name'])
+    def join_room(self, client_connection, client_request):
+        ## ルームが存在するか確認
+        if self.chat_room_list.check_room_name(client_request['room_name']) :
+            chat_room = self.chat_room_list.get_room(client_request['room_name'])
+            # パスワードが一致するか確認
+            if chat_room.is_password_checked(client_request["password"]):
                 chat_room.user_list.add_user(client_request['user_name'])
-                print(chat_room.user_list.get_host_user())
-                print(chat_room.user_list.get_guest_users())
-
                 client_request['state'] = 1
+                client_request["roomPassword"] = ""
+                client_request['token'] = client_request['user_name'] + self.generate_token()
                 self.tcp.send_request(client_connection, client_request)
                 client_request = self.tcp.receive_message(client_connection)
-
-                # self.get_room(room_name).add_client_info(user)
-                # print(f"{user_name}が{room_name}に参加しました")
-
-        elif client_request['operation'] == 2:
-            # join room
-            if self.chat_room_list.check_room_name(client_request['room_name']):
-                chat_room = self.chat_room_list.get_room(
-                    client_request['room_name'])
-
-                chat_room.user_list.add_user(client_request['user_name'])
-
-                print(chat_room.user_list.get_host_user())
-                print(chat_room.user_list.get_guest_users())
-
-                client_request['state'] = 1
-                self.tcp.send_request(client_connection, client_request)
-                client_request = self.tcp.receive_message(client_connection)
-        else:
-            pass
-
-        # user_name  = response_dict["user_name"]
-        # user = User(udp_addr=None, tcp_addr=None,  response_dict["user_name"],is_host = False)
-        # self.clients[user_name] = user
-        # self.tokens[user_name] = user.join_token
-        #
-        # room_name = response_dict["room_name"]
-        # if not self.check_roomname(room_name):
-        #     self.add_room(room_name)
-        #     self.set_password(response_dict["password"])
-        #     user.is_host = True
-        #     self.get_room(room_name).add_client_info(user)
-        #     print(f"{user_name}が{room_name}に参加しました")
-        # else:
-        #     if not self.get_room(room_name).is_password_checked(response_dict["password"]):
-        #         print("パスワードが違います")
-        #     else:
-        #         self.get_room(room_name).add_client_info(user)
-        #         print(f"{user_name}が{room_name}に参加しました")
 
     def start_udp_server(self):
         self.udp = UDPServer(HOST, PORT)
@@ -145,3 +120,6 @@ class ServerModel:
             if client_token != sender_token:  # 送信者自身には送らない
                 data_bytes = json.dumps(message).encode('utf-8')
                 self.udp.socket.sendto(data_bytes, hash_map_token_udp[client_token])
+
+    def generate_token(self):
+        return uuid.uuid4().hex
